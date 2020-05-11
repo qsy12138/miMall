f57aa9bbfd47d8f705716de76c406f96f8ae703d
QianSiyuan <1051335932@qq.com>
2020-05-11 16:27:18 +0800

    网页头部开发

--- src/App.vue
+++ src/App.vue
@@ -9,30 +9,10 @@ export default {
   name: 'App',
   components: {
     
-  },
-  data() {
-    return {
-      res:{}
-    }
-  },
-  mounted(){
-    // this.axios.get('/mock/user/login.json').then((res)=>{ //本地加载请求静态JSON文件的形式调用mock
-    //   this.res = res
-    // })
-    this.axios.get('/mock/user/login.json').then((res)=>{ //使用mockjs的方法请求模拟数据
-      this.res = res
-    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ import VueAxios from 'vue-axios'
 import App from './App.vue'
 
 //MockJs开关，当我们使用mockjs的方法时，必须要配置该方法
-const mock = true
+const mock = false;
 if(mock){
   require('./mock/api') //这里强调使用require的方式加载js文件，import属于预编译加载，如果使用import的，请求就会永远被拦截，而require属于编译时加载，它会去看这个if判断，根据if判断决定是否加载
 }
 
 //根据前端的跨域方式做调整
-//axios.defaults.baseURL = '/api' //当跨域方式为接口代理时，使用/api这种方式作为baseURL，需要注意的是，当我们不与后台进行接口联调的而使用mockjs的时候，baseURL要注释掉
+axios.defaults.baseURL = '/api' //当跨域方式为接口代理时，使用/api这种方式作为baseURL，需要注意的是，当我们不与后台进行接口联调的而使用mockjs的时候，baseURL要注释掉
 axios.defaults.timeout = 8000 //默认的请求超时时间
 
 //接口错误拦截
